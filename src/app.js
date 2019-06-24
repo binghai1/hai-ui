60560da3f756cbf67db52b30ea520ac070854f08
“binghai1” <448456376@qq.com>
2019-06-24 12:12:26 +0800

    chai 单元测试

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import Hicon from './compoents/icon'
 Vue.component('h-button',Button)
 Vue.component('h-icon',Hicon)
 Vue.component('button-group',buttonGroup)
-console.log(Hicon)
 new Vue({
         el:'#app',
         data(){
@@ -15,3 +14,29 @@ new Vue({
                 }
         }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+{       
+        let Constructor= Vue.extend(Button)
+        let vm=new Constructor({propsData:{icon:"settings"}})
+        vm.$mount()
+        let element=vm.$el.querySelector('use')
+        let href=element.getAttribute('xlink:href')
+        expect(href).to.eq('#i-settings')
+        vm.$el.remove()
+        vm.$destroy()
+}
+// {       
+//         let Constructor=Vue.extend(Button)
+//         let  vm = new  Constructor({propsData:{
+//                 icon:'download',
+//                 iconPosition:'right'
+//         }})
+//         vm.$mount('div')
+//         console.log(vm.$el)
+
+//         let element=vm.$el.querySelector('svg')
+//         let {order} = window.getComputedStyle(element)
+//         expect(order).to.eq('2')
+// } 
